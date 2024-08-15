ff213445f57ed5cd76c5d4c45bf41d1d9f099d95
Huy TRAN <huy.tran@lundimatin.fr>
2024-08-15 10:10:37 +0700

    demo admin page

--- todo/urls.py
+++ todo/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.AllToDos.as_view(), name="index")
+    path("", views.AllToDos.as_view(), name="index"),
+    path("today/", views.TodayToDos.as_view(), name="today")
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from django.views.generic import ListView
 from .models import ToDoItem
 
@@ -5,3 +7,14 @@ from .models import ToDoItem
 class AllToDos(ListView):
     model = ToDoItem
     template_name = "todo/index.html"
+
+    def get_queryset(self):
+        return ToDoItem.objects.filter(due_date__gte=date.today())
+
+
+class TodayToDos(ListView):
+    model = ToDoItem
+    template_name = "todo/today.html"
+
+    def get_queryset(self):
+        return ToDoItem.objects.filter(due_date=date.today())
