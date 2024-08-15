cc86331e042c748028d504a4cdeb31a7d7dfbe69
Huy TRAN <huy.tran@lundimatin.fr>
2024-08-15 09:49:52 +0700

    demo todo app

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from .models import ToDoItem
 
-# Create your views here.
+
+class AllToDos(ListView):
+    model = ToDoItem
+    template_name = "todo/index.html"
